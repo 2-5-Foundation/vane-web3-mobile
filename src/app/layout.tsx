1dce0c3be34242b53e4a35192c8839796a8121c2
MrishoLukamba <abdulrazzaqlukamba@gmail.com>
2026-01-04 08:12:46 +0300

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { PWAProvider } from '@/app/page-view/pwa-provider'
 import { Frame } from '@/app/page-view/frame'
@@ -99,6 +100,19 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
       >
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-01YWN77974"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-01YWN77974');
+          `}
+        </Script>
         <DesktopCheck>
           <DynamicWalletClientAuthProvider> 
             <PWAProvider />
